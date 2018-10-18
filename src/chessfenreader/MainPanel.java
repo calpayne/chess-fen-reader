9ea4dc366ba446f1513c0914946066f50fc1e86e
calpayne <34384159+calpayne@users.noreply.github.com>
2018-10-18 19:07:00 +0100

    Added isValidFen method

--- src/chessfenreader/MainPanel.java
+++ src/chessfenreader/MainPanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -27,8 +28,10 @@ public class MainPanel extends JPanel {
         JTextField fen = new JTextField("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -", 40);
         JButton submit = new JButton("Set Fen");
         submit.addActionListener((ActionEvent ae) -> {
-            if(isValidFen(fen.getText())) {
+            if (isValidFen(fen.getText())) {
                 renderFen(fen.getText());
+            } else {
+                JOptionPane.showMessageDialog(null, "Please use a valid fen string", "Error", JOptionPane.ERROR_MESSAGE);
             }
         });
 
@@ -38,19 +41,46 @@ public class MainPanel extends JPanel {
         this.setBackground(Color.WHITE);
         this.setPreferredSize(new Dimension(650, 660));
     }
-    
+
+    /**
+     * Validates a fen
+     *
+     * @param fen the fen to validate
+     * @return if the fen is valid
+     */
     public boolean isValidFen(String fen) {
+        if (fen.chars().filter(num -> num == '/').count() != 7) {
+            return false;
+        }
+
+        fen = fen.substring(0, fen.indexOf(" "));
+        String[] check = fen.split("/");
+        for (String s : check) {
+            int total = 0;
+            for (char c : s.toCharArray()) {
+                if (Character.isLetter(c)) {
+                    total++;
+                } else {
+                    total += Integer.parseInt(String.valueOf(c));
+                }
+            }
+
+            if (total != 8) {
+                return false;
+            }
+        }
+
         return true;
     }
 
     /**
      * Renders a fen onto the screen
-     * 
+     *
      * @param fen the fen to render
      */
     public void renderFen(String fen) {
         boardContainer.removeAll();
-        
+
         fen = fen.substring(0, fen.indexOf(" ")).replaceAll("/", "");
         char[] fenArray = fen.toCharArray();
 
@@ -75,7 +105,7 @@ public class MainPanel extends JPanel {
             }
             isLight = !isLight;
         }
-        
+
         boardContainer.revalidate();
         boardContainer.repaint();
     }
